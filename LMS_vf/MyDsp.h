60fc64ab35d0786204bb540b0fa2d296363a2e65
mohamed-set <152507345+mohamed-set@users.noreply.github.com>
2024-02-20 20:59:35 +0100

    Create MyDsp.h

---
+++ LMS_vf/MyDsp.h
@@ -0,0 +1,40 @@
+#ifndef faust_teensy_h_
+#define faust_teensy_h_
+#define NUM_COEFFICIENTS 2
+
+#include "Arduino.h"
+#include "AudioStream.h"
+#include "Audio.h"
+
+#include "Noise.h"
+#include "OneZero.h"
+
+class MyDsp : public AudioStream
+{
+  public:
+    MyDsp();
+    ~MyDsp();
+    
+    virtual void update(void);
+    void LMS(float sample);
+    void pushInput(float sample);
+    void outputSys();
+    void adaptativeFilter();
+    void outputError();
+    void correctionCoef();
+    int boutonPin;
+    bool boutonEtat;
+    bool lireEtatBouton();
+    
+  private:
+    Noise noise;
+    OneZero oneZero;
+    float mu ; 
+    float adaptSignal;
+    float error ;
+    float output_H ;
+    float input[NUM_COEFFICIENTS] ;
+    float coefficients[NUM_COEFFICIENTS];
+};
+
+#endif
