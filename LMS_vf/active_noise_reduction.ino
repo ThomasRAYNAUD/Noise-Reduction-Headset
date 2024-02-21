c17071539b7420ae27b11e42d907213e10157356
mohamed-set <152507345+mohamed-set@users.noreply.github.com>
2024-02-21 11:16:26 +0100

    Update active_noise_reduction.ino

--- LMS_vf/active_noise_reduction.ino
+++ LMS_vf/active_noise_reduction.ino
@@ -5,17 +5,16 @@ MyDsp myDsp;
 AudioOutputI2S out;
 AudioControlSGTL5000 audioShield;
 AudioConnection patchCord0(myDsp,0,out,0);
-AudioConnection patchCord1(myDsp,0,out,1);
 
 void setup() {
   Serial.begin(9600);
   AudioMemory(6);
   audioShield.enable();
+  audioShield.volume(0.3);
+  audioShield.micGain(10);
   
 }
 
 void loop() {
-  int sensorValue = analogRead(A0);
-  audioShield.volume(sensorValue*0.00088);
 }
 
